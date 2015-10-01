04ff60f0022bdabf26bf5e5c735669ac32ea4d08
zongyuwu <w.zongyu@gmail.com>
2015-10-01 14:16:04 +0800

    modify

--- dnssecserver/bin/only_query/server/probe.rb
+++ dnssecserver/bin/only_query/server/probe.rb
@@ -2,7 +2,8 @@
 
 class Usage_Probe
   def initialize
-    @@target = "named"
+    #@@target = "named"
+    @@target = "Xorg"
     @@period = 0.001
     @@totall = 100
     @CPU_usage = []
@@ -32,6 +33,11 @@ class Usage_Probe
     end
   end
 
+  def draw filetodraw
+    draw_core("CPU", "#{filetodraw}_CPU.gif")
+    draw_core("MEM", "#{filetodraw}_MEM.gif")
+  end
+
 private
   def probe_core
     r = `top -n 1`
@@ -39,9 +45,37 @@ private
     if !tmp.nil?
       @CPU_usage << tmp[1]
       @MEM_usage << tmp[2]
-    else
-      @CPU_usage << 0
-      @MEM_usage << 0
+    #else
+      #@CPU_usage << 0
+      #@MEM_usage << 0
+    end
+  end
+
+  def draw_core type, filetodraw
+    if type == "CPU"
+      len = @CPU_usage.length
+      y = @CPU_usage
+    elsif type == "MEM"
+      len = @MEM_usage.length
+      y = @MEM_usage
+    end
+    x = (1..len).to_a
+    title = "#{type} Usage"
+    xlabel = "probe squence"
+    ylabel = "Percentage"
+    require 'gnuplot'
+    Gnuplot.open do |gp|
+      Gnuplot::Plot.new(gp) do |plot|
+        plot.terminal "gif"
+        plot.output filetodraw
+        plot.title title
+        plot.ylabel ylabel
+        plot.xlabel xlabel
+        plot.data << Gnuplot::DataSet.new( [x, y] ) do |ds|
+          ds.with = "linespoints"
+          ds.title = "#{type} Usage"
+        end
+      end
     end
   end
 end
@@ -49,5 +83,6 @@ end
 prb = Usage_Probe.new
 prb.probe
 puts "done probing"
-prb.write_cpu_res "/tmp/test"
-prb.write_mem_res "/tmp/test"
+prb.write_cpu_res "../../../res/only_query/server"
+prb.write_mem_res "../../../res/only_query/server"
+prb.draw "../../../res/only_query/server"
