da2b66c0f047485256c87c3f9d84df9c7f2012ab
zongyuwu <w.zongyu@gmail.com>
2015-10-02 12:00:15 +0800

    avarage script

---
+++ dnssecserver/bin/only_query/server/average.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+
+path = "../../../res/only_query/server_mem_100"
+sum = 0.0
+i = 0
+File.open(path, "r").each_line do |line|
+  i += 1
+  sum += line.to_i
+end
+
+puts "average = #{sum/i}"
